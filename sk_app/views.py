afc4cce358f75bb26120309027a0bbb43d812e87
kaizu <hayato31halit@gmail.com>
2024-02-16 15:16:44 +0900

    構成変更

--- sk_app/views.py
+++ sk_app/views.py
@@ -21,26 +21,3 @@ app.register_blueprint(admin_view)
 #====================================================
 app.register_blueprint(test_view)
 
-
-
-
-
-# パスの設定
-# user = "user/"
-# admin = "admin/"
-
-#「/user」へアクセスがあった場合に、「user/index.html」を返す
-# @app.route("/user")
-# def user_index():
-#     return render_template(user + "index.html")
-
-#「/admin」へアクセスがあった場合に、「admin/index.html」を返す
-# @app.route("/admin")
-# def admin_index():
-#     return render_template(admin + "index.html")
-
-
-#「/test」へアクセスがあった場合に、"Hello World"の文字列を返す
-# @app.route("/test")
-# def test():
-#     return "Hello, World!"
